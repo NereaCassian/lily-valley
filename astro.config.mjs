10e506ff259a0b26983fd8784944c5ccd9849aac
Nerea Kalandadze <nerea.kalandadze@makingscience.com>
2024-10-21 16:11:46 +0200

    chore: adapted the template to my needs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+      remarkPlugins: [
+        ['remark-breaks', {}], // Habilita la interpretación de newlines como <br> o saltos de línea.
+      ],
+    },
+  });
